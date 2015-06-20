0495d1a6fb565a8537bd4809a4c0c2d7947dafc7
Tom Remeeus <tomremeeus@hotmail.com>
2015-06-20 10:26:38 +0200

    User can now shoot the ball, only if the ball is attached to the controlled player.

--- src/app/SoccerController.java
+++ src/app/SoccerController.java
@@ -381,7 +381,7 @@ class SoccerController extends WiimoteAdapter implements Runnable
             else if (e.isButtonRightJustReleased())
                 remote.releaseButton(WiimoteButton.RIGHT);
 
-            else if (e.isButtonOneJustPressed()) {
+            else if (e.isButtonOneJustPressed() && controlledFieldPlayer.playerEllipse.contains(view.getBall().getBall())) {
                 view.getBall().accelerate(40, controlledFieldPlayer.getAngle());
                 System.out.println(controlledFieldPlayer.getAngle());
             }
